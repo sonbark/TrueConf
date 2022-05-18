5277e92fa0c7d2771e759095db42fdda06e972c2
sonya banzaraktsaeva <sofabark@gmail.com>
2022-05-18 20:02:54 +0300

    2. При клике на экран, переместить текст в точку касания (без анимации) и поменять цвет текста в английской локали на красный, в русской на синий.

--- app/src/main/java/com/banzaraktsaeva/trueconf/MainActivity.java
+++ app/src/main/java/com/banzaraktsaeva/trueconf/MainActivity.java
@@ -1,14 +1,45 @@
 package com.banzaraktsaeva.trueconf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    float x, y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+
+        //View главной активити привязывется к ConstraintLayout по id
+        View mainActivity = findViewById(R.id.screen);
+
+        //OnTouchListener "прослушивает" весь экран MainActivity
+        mainActivity.setOnTouchListener((view, motionEvent) -> {
+            view.performClick();
+
+            x = motionEvent.getX();
+            y = motionEvent.getY();
+
+            //При отпускании нажатия textView перемещается в координаты x, y и меняет цвет
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                textView.setX(x);
+                textView.setY(y);
+
+                //Цвет меняется в зависимости от языка системы
+                textView.setTextColor(ContextCompat.getColor(this, R.color.move));
+            }
+            return true;
+        });
     }
 }
