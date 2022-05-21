7e1f739ed850e3951ca196d3305b5f35e161bb36
sonya banzaraktsaeva <sofabark@gmail.com>
2022-05-21 14:11:58 +0300

    3. После того как текст переместили, через 5 секунд начать анимированное перемещение текста вниз экрана (следующий клик по экрану сбрасывает время старта анимации и перемещает текст по новым координатам) - сделано
    При касании текстом экрана снизу, начать его
    подъем, при касании текстом экрана сверху, начать спуск и тд. - текст уезжает за границы экрана

--- app/src/main/java/com/banzaraktsaeva/trueconf/MainActivity.java
+++ app/src/main/java/com/banzaraktsaeva/trueconf/MainActivity.java
@@ -3,23 +3,29 @@ package com.banzaraktsaeva.trueconf;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    float x, y;
+    Animation downAnimation;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.text_view);
+
+        downAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.down_translate);
 
         //View главной активити привязывется к ConstraintLayout по id
         View mainActivity = findViewById(R.id.screen);
@@ -28,18 +34,54 @@ public class MainActivity extends AppCompatActivity {
         mainActivity.setOnTouchListener((view, motionEvent) -> {
             view.performClick();
 
-            x = motionEvent.getX();
-            y = motionEvent.getY();
+            float x = motionEvent.getX();
+            float y = motionEvent.getY();
 
-            //При отпускании нажатия textView перемещается в координаты x, y и меняет цвет
-            if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
-                textView.setX(x);
-                textView.setY(y);
-
-                //Цвет меняется в зависимости от языка системы
-                textView.setTextColor(ContextCompat.getColor(this, R.color.move));
+            //Если нажатие не по тексту, то
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP & x != textView.getX() & y != textView.getY()){
+                //При отпускании нажатия textView перемещается в координаты x, y и меняет цвет
+                moveMethod(motionEvent);
+            } else {
+                downAnimation.cancel();
             }
+
             return true;
         });
+    }
+
+    public synchronized void moveMethod(MotionEvent motionEvent){
+        float x = motionEvent.getX();
+        float y = motionEvent.getY();
+
+        textView.setX(x);
+        textView.setY(y);
+
+        //Цвет меняется в зависимости от языка системы
+        textView.setTextColor(ContextCompat.getColor(this, R.color.move));
+
+        animationMethod();
+    }
+
+    public synchronized void animationMethod(){
+        //Анимация движения вниз/вверх
+        downAnimation.setStartOffset(5000);
+
+        Animation.AnimationListener downAnimationListener = new Animation.AnimationListener(){
+
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) { }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                animation.setStartOffset(0);
+            }
+        };
+
+        downAnimation.setAnimationListener(downAnimationListener);
+
+        textView.startAnimation(downAnimation);
     }
 }
